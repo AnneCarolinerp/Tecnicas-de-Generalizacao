bf1e81287db87f0fca41dbf9cef2a4a34000a0b7
Anne Caroline <caroline.annerp@gmail.com>
2022-07-02 21:43:29 -0300

    Técnicas de Anonimização
    Ajuste na formatação do código

--- T_Generalização.py
+++ T_Generalização.py
@@ -1,1 +1,51 @@
-{"cells":[{"cell_type":"code","execution_count":null,"metadata":{"id":"Rbtipp_oRIvx"},"outputs":[],"source":["from pandas.core.frame import DataFrame\n","from google.colab.patches import cv2_imshow\n","import pandas as pd\n","import math\n","import cv2\n","\n","corona = pd.read_csv(\"Covid.csv\", low_memory=False)\n","regioes = pd.read_csv(\"Regioes.csv\")\n","\n","corona = corona[['municipioCaso', 'sexoCaso', 'dataNascimento', 'resultadoFinalExame']]\n","corona = corona.dropna()\n","\n","#@title Técnicas de Anonimização – Generalização {run:'auto'}\n","\n","#@markdown Informe o nível desejado para visualização da data de nascimento:\n","nivelDN = 'Nivel 1' #@param [\"Nivel 1\", \"Nivel 2\", \"Nivel 3\"]\n","\n","#@markdown Informe o nível desejado para visualização do municipio:\n","nivelEst = 'Nivel 1' #@param [\"Nivel 1\", \"Nivel 2\"]\n","\n","#@markdown Representação da árvore:\n","img = cv2.imread(\"RepresentacaoDeArvore.png\")\n","cv2_imshow(img)\n","\n","lines = []\n","\n","for index, row in corona[:10].iterrows():\n","  if(type(row['dataNascimento'])==str):\n","    dataNasc = row['dataNascimento'].split('-')\n","    if nivelDN == \"Nivel 3\" :\n","      row['dataNascimento'] = dataNasc[2]+'/'+dataNasc[1]+'/'+dataNasc[0]\n","    elif nivelDN == \"Nivel 2\" :\n","      row['dataNascimento'] = dataNasc[1]+'/'+dataNasc[0]\n","    elif nivelDN == \"Nivel 1\" :\n","      row['dataNascimento'] = dataNasc[0]\n","  if(type(row['municipioCaso']) == str):\n","    if nivelEst == \"Nivel 2\" :\n","      row['municipioCaso'] = row['municipioCaso']\n","      dataframe = pd.DataFrame(lines, columns=['Data', 'Municipio', 'Resultado Exame'])\n","\n","    elif nivelEst == \"Nivel 1\" :\n","      row['municipioCaso'] = regioes.query('municipios == \"'+ row['municipioCaso'] + '\"')['regioes'].values[0]\n","      dataframe = pd.DataFrame(lines, columns=['Data', 'Região', 'Resultado Exame'])\n","\n","  result = (row['dataNascimento'], row['municipioCaso'], row['resultadoFinalExame'])\n","  lines.append(result)\n","\n","dataframe.to_csv('covid_publico')\n","for coluna in dataframe.columns:\n","  display(dataframe.sort_values(coluna))\n","  break"]}],"metadata":{"colab":{"authorship_tag":"ABX9TyOLknJRbShOg0IjVSs71SfG","collapsed_sections":[],"mount_file_id":"1hkB04CKyI267amdboC429EKh77Lnnh6c","name":"T_Generalização.ipynb","provenance":[]},"kernelspec":{"display_name":"Python 3","name":"python3"},"language_info":{"name":"python"}},"nbformat":4,"nbformat_minor":0}
+from pandas.core.frame import DataFrame
+from google.colab.patches import cv2_imshow
+import pandas as pd
+import math
+import cv2
+
+corona = pd.read_csv("Covid.csv", low_memory=False)
+regioes = pd.read_csv("Regioes.csv")
+
+corona = corona[['municipioCaso', 'sexoCaso', 'dataNascimento', 'resultadoFinalExame']]
+corona = corona.dropna()
+
+#@title Técnicas de Anonimização – Generalização {run:'auto'}
+
+#@markdown Informe o nível desejado para visualização da data de nascimento:
+nivelDN = 'Nivel 1' #@param ["Nivel 1", "Nivel 2", "Nivel 3"]
+
+#@markdown Informe o nível desejado para visualização do municipio:
+nivelEst = 'Nivel 1' #@param ["Nivel 1", "Nivel 2"]
+
+#@markdown Representação da árvore:
+img = cv2.imread("RepresentacaoDeArvore.png")
+cv2_imshow(img)
+
+lines = []
+
+for index, row in corona[:10].iterrows():
+  if(type(row['dataNascimento'])==str):
+    dataNasc = row['dataNascimento'].split('-')
+    if nivelDN == "Nivel 3" :
+      row['dataNascimento'] = dataNasc[2]+'/'+dataNasc[1]+'/'+dataNasc[0]
+    elif nivelDN == "Nivel 2" :
+      row['dataNascimento'] = dataNasc[1]+'/'+dataNasc[0]
+    elif nivelDN == "Nivel 1" :
+      row['dataNascimento'] = dataNasc[0]
+  if(type(row['municipioCaso']) == str):
+    if nivelEst == "Nivel 2" :
+      row['municipioCaso'] = row['municipioCaso']
+      dataframe = pd.DataFrame(lines, columns=['Data', 'Municipio', 'Resultado Exame'])
+
+    elif nivelEst == "Nivel 1" :
+      row['municipioCaso'] = regioes.query('municipios == "'+ row['municipioCaso'] + '"')['regioes'].values[0]
+      dataframe = pd.DataFrame(lines, columns=['Data', 'Região', 'Resultado Exame'])
+
+  result = (row['dataNascimento'], row['municipioCaso'], row['resultadoFinalExame'])
+  lines.append(result)
+
+dataframe.to_csv('covid_publico')
+for coluna in dataframe.columns:
+  display(dataframe.sort_values(coluna))
+  break
